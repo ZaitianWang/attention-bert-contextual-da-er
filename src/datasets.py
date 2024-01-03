7718fa85910335f8ac5285e5d0dcb91c5160f5b1
XiaohanHwang <58513006+XiaohanHwang@users.noreply.github.com>
2024-01-03 17:11:58 +0800

    Support context length

--- src/datasets.py
+++ src/datasets.py
@@ -9,22 +9,11 @@ import numpy as np
 
 class ContextualDataset(Dataset):
     # def __init__(self, tokenizer, max_len, mode='train'):
-    def __init__(self, data, label, tokenizer, max_len, mode='train'):
+    def __init__(self, data, label, tokenizer, max_len,context_len=5):
         super(ContextualDataset, self).__init__()
-        if mode == 'train':
-            # file = './train_data.txt'
-            # with open(file,'r') as f:
-            #     text_list = f.readlines()
-            #     self.data = [line.strip() for line in text_list]
-                
-            # file = './train_label.txt'
-            # with open(file,'r') as f:
-            #     label_list = f.readlines()
-            #     self.label = [line.strip() for line in label_list]
-            self.data = data
-            self.label = label
-        else:
-            self.data = pd.read_csv('data/test.csv',sep='\t')
+        self.data = data
+        self.label = label
+        self.context_len = context_len
             
         self.tokenizer = tokenizer
         self.max_len = max_len
@@ -38,18 +27,18 @@ class ContextualDataset(Dataset):
         for k,label_col in enumerate(target_cols):
             sample[label_col] = torch.tensor(int(labels[k])/3.0,dtype=torch.float)
 
-        win_sen = self.data[index].split(', ')
-        if(len(win_sen)!=7):
-            # print("error")
-            # only keep first 7 sentences
-            win_sen = win_sen[:7]
-        #each[1:-1]去前后引号，空白格
-        for i,each in enumerate(win_sen):
+        sentences = self.data[index].split(', ')
+        win_sen = [sentences[0]]
+        con_len = self.context_len
+
+        start = int(len(sentences) / 2) - con_len
+        # input_seq = [0]
+        for i in range(con_len*2):
+            win_sen.append(sentences[start+i+1])
+        # print(input_seq)
+
+        for each in win_sen:
             each = each[1:-1]
-            # if(i==0):
-            #     each = each[1:-1]
-            # else:
-            #     each = each[2:-1]
             encoding = self.tokenizer.encode_plus(each,
                                             add_special_tokens=True, # 加上开头结尾符
                                             truncation=True,
@@ -69,7 +58,3 @@ class ContextualDataset(Dataset):
         return len(self.data)
     
     
-# PRE_TRAINED_MODEL_NAME='hfl/chinese-roberta-wwm-ext'  # 'hfl/chinese-roberta-wwm-ext'
-# tokenizer = BertTokenizer.from_pretrained(PRE_TRAINED_MODEL_NAME)
-# trainset = RoleDataset(tokenizer, max_len=128, mode='train')
-# dataloader = DataLoader(trainset)
